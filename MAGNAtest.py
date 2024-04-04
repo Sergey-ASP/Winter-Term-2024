0205e7f9b4df067655328ed959910ccca4d2dfab
Trololisa007 <92666886+Trololisa007@users.noreply.github.com>
2024-04-03 23:25:23 -0400

    4/3

--- MAGNAtest.py
+++ MAGNAtest.py
@@ -5,16 +5,18 @@ import k3d as k3d
 import magna.utils as mu
 
 #  use r_tuple to specify radius of spheres
-magna = mu.MNP(id=-1, name="test", n_layers=1, layer_radius=1, discretizations=(15,15,15), r_tuple=(24e-9, 3e-9, 3e-9), layer_dims=(1,2), shape='rectangle', axes_type='random_plane') #disc was set to all 10 initially
+# magna = mu.MNP(id=-1, name="test", n_layers=1, layer_radius=3, discretizations=(15,15,15), r_tuple=(4e-9, 3e-9, 3e-9)) #, axes_type='random_plane') #disc was set to all 10 initially
 # magna.save_fields();
+magna = mu.MNP(id=-1)
 mu.quick_drive(magna)
 M, A, K, U = magna.maku()
 
 M.sel('z').mpl();
 M.sel('x').mpl();
-# plt.show();
+M.sel('y').mpl();
+plt.show();
 
-M.to_file("magnaTestData_R_24e-9ORIGINAL.omf", representation="txt")
+# M.to_file("magnaTestData_d10_r30_l3.omf", representation="txt")
 
 
 
